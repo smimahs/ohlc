b78731a52664fce2968a417f87da92880f0fbcee
Shamim Sanisales <shamims12@yahoo.com>
2023-03-13 13:27:32 +0330

    update

--- app/api/api.go
+++ app/api/api.go
@@ -5,7 +5,6 @@ import (
 	"fmt"
 	"os"
 
-	"app/config"
 	pq "github.com/lib/pq"
 
 	"bufio"
@@ -18,29 +17,6 @@ import (
 	//"net/http"
 )
 
-func Connect() (*sql.DB, error) {
-	// Get database connection string from environment variable
-	config.Load()
-	connectionString := os.Getenv("DATABASE_URL")
-	if connectionString == "" {
-		return nil, fmt.Errorf("DATABASE_URL environment variable not set")
-	}
-
-	// Open database connection
-	db, err := sql.Open("postgres", connectionString)
-	if err != nil {
-		return nil, fmt.Errorf("failed to open database connection: %v", err)
-	}
-
-	// Test database connection
-	if err = db.Ping(); err != nil {
-		db.Close()
-		return nil, fmt.Errorf("failed to ping database: %v", err)
-	}
-
-	fmt.Println("Successfully connected!")
-	return db, nil
-}
 
 func Update_API(app *iris.Application, db *sql.DB) {
 	// Define a handler for POST requests to "/update"
